1ff24ab10b25475dee6c4a5eb8c6c6119402695c
zoro <zoro@qinarmy.io>
2023-12-12 18:31:30 +0800

    fix java doc

--- army-core/src/main/java/io/army/criteria/QualifiedField.java
+++ army-core/src/main/java/io/army/criteria/QualifiedField.java
@@ -5,7 +5,8 @@ package io.army.criteria;
  * <p>
  * This interface representing qualified field , output format: tableAlias.column .
  * You don't need a {@link QualifiedField},if no self-join in statement.
- * * @param <T> java type domain.
+ *
+ * @param <T> java type domain.
  */
 public interface QualifiedField<T> extends TypeTableField<T> {
 
--- army-core/src/main/java/io/army/meta/SingleTableMeta.java
+++ army-core/src/main/java/io/army/meta/SingleTableMeta.java
@@ -5,11 +5,12 @@ package io.army.meta;
 /**
  * <p>
  * This interface is base interface of below:
- *     <ul>
- *         <li>{@link SimpleTableMeta}</li>
- *         <li>{@link ParentTableMeta}</li>
- *     </ul>
- * * @param <T> domain java type
+ * <ul>
+ *     <li>{@link SimpleTableMeta}</li>
+ *     <li>{@link ParentTableMeta}</li>
+ * </ul>
+ *
+ * @param <T> domain java type
  */
 public interface SingleTableMeta<T> extends TableMeta<T> {
 
