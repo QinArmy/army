a2732f862fa22288acb795acff8e46e9ec81a614
zoro <zoro@qinarmy.io>
2024-06-25 02:29:08 +0800

    modify project java version to java 21

--- army-postgre/src/main/java/io/army/dialect/PostgreMetaStmtGenerator.java
+++ army-postgre/src/main/java/io/army/dialect/PostgreMetaStmtGenerator.java
@@ -31,29 +31,31 @@ final class PostgreMetaStmtGenerator implements MetaStmtGenerator {
     public String queryUserDefinedTypeStmts() {
         // 1. 确定 自定义 composite 的方法 : pg_type.typcategory is 'C' 且 pg_class.relkind is 'c'
         // 2. 确定 domain 的方法 pg_type.typbasetype > 0
-        return "SELECT t.typname AS \"typeName\", t.typcategory AS \"type\", et.\"enumLabelArray\", at.*\n" +
-                "FROM pg_namespace AS n\n" +
-                "         JOIN pg_type AS t ON t.typnamespace = n.oid\n" +
-                "         LEFT JOIN LATERAL (\n" +
-                "    SELECT e.enumtypid, array_agg(e.enumlabel) AS \"enumLabelArray\"\n" +
-                "    FROM pg_enum AS e\n" +
-                "    WHERE e.enumtypid = t.oid\n" +
-                "    GROUP BY e.enumtypid\n" +
-                "    ) AS et ON et.enumtypid = t.oid\n" +
-                "         LEFT JOIN pg_class AS c ON c.oid = t.typrelid\n" +
-                "         LEFT JOIN LATERAL (\n" +
-                "    SELECT a.attrelid,\n" +
-                "           array_agg(a.attnum)   AS \"columnNumArray\",\n" +
-                "           array_agg(a.attname)  AS \"columnNameArray\",\n" +
-                "           array_agg(st.typname) AS \"columnTypeArray\"\n" +
-                "    FROM pg_attribute AS a\n" +
-                "             JOIN pg_type AS st ON st.oid = a.atttypid\n" +
-                "    WHERE a.attrelid = c.oid\n" +
-                "    GROUP BY a.attrelid\n" +
-                "    ) AS at ON at.attrelid = c.oid\n" +
-                "\n" +
-                "WHERE n.nspname NOT IN ('pg_catalog', 'information_schema')\n" +
-                "  AND (t.typcategory IN ('E', 'U', 'R') OR t.typbasetype > 0 OR (t.typcategory = 'C' AND c.relkind = 'c'))";
+        return """
+                SELECT t.typname AS "typeName", t.typcategory AS "type", et."enumLabelArray", at.*
+                FROM pg_namespace AS n
+                         JOIN pg_type AS t ON t.typnamespace = n.oid
+                         LEFT JOIN LATERAL (
+                    SELECT e.enumtypid, array_agg(e.enumlabel) AS "enumLabelArray"
+                    FROM pg_enum AS e
+                    WHERE e.enumtypid = t.oid
+                    GROUP BY e.enumtypid
+                    ) AS et ON et.enumtypid = t.oid
+                         LEFT JOIN pg_class AS c ON c.oid = t.typrelid
+                         LEFT JOIN LATERAL (
+                    SELECT a.attrelid,
+                           array_agg(a.attnum)   AS "columnNumArray",
+                           array_agg(a.attname)  AS "columnNameArray",
+                           array_agg(st.typname) AS "columnTypeArray"
+                    FROM pg_attribute AS a
+                             JOIN pg_type AS st ON st.oid = a.atttypid
+                    WHERE a.attrelid = c.oid
+                    GROUP BY a.attrelid
+                    ) AS at ON at.attrelid = c.oid
+                                
+                WHERE n.nspname NOT IN ('pg_catalog', 'information_schema')
+                  AND (t.typcategory IN ('E', 'U', 'R') OR t.typbasetype > 0 OR (t.typcategory = 'C' AND c.relkind = 'c'))
+                """;
     }
 
 
