a2c66f2e7795dd7dc3a3cc09723e45acb9824e5b
zoro <zoro@qinarmy.io>
2024-07-19 21:14:21 +0800

    modify SyncCriteria.java

--- army-sync/src/main/java/io/army/util/SyncCriteria.java
+++ army-sync/src/main/java/io/army/util/SyncCriteria.java
@@ -9,8 +9,21 @@ public abstract class SyncCriteria {
         throw new UnsupportedOperationException();
     }
 
+
+    public static <T> long rowCount(Class<T> domainClass, SyncSession session) {
+        return rowCountOf(session.tableMeta(domainClass), session);
+    }
+
     public static <T> long rowCountOf(TableMeta<T> table, SyncSession session) {
         return session.queryOne(SQLStmts.rowCountStmtOf(table), Long.class);
+    }
+
+    public static <T> int rowCountAsInt(Class<T> domainClass, SyncSession session) {
+        return rowCountAsIntOf(session.tableMeta(domainClass), session);
+    }
+
+    public static <T> int rowCountAsIntOf(TableMeta<T> table, SyncSession session) {
+        return session.queryOne(SQLStmts.rowCountStmtOf(table), Integer.class);
     }
 
 
